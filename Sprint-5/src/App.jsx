f963e4ed08f5f6861bd4637c02d78aa934802409
I Mマリベル <immaribel7@gmail.com>
2024-09-18 12:40:18 +0200

    Ej2- nextStep()

--- Sprint-5/src/App.jsx
+++ Sprint-5/src/App.jsx
@@ -6,15 +6,36 @@ function App() {
     { title: 'Dedica moltes hores', 
       description: `Un mínim de 30 hores a la semana. Si no en tens prou,
       hauràs de dedicar-li més hores. Al principi sembla impossible,
-       però notaràs una millora ràpidament.` }
+       però notaràs una millora ràpidament.` },
+
+    { title: 'Programa projectes propis',
+      description: `Més val 10 hores treballant en projectes propis, 
+      que 10 hores mirant tutorials. La motivació i la implicació en 
+      el projecte ajudarà a accelerar el teu aprenentatge.`
+    },
+
+    { title: 'Procura descansar',
+      description: `Descansa bé i desconectar són vitals. D'aquesta manera reduiràs
+      l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu
+      aprenentatge.`
+    }
   ];
 
   const [step, setStep] = useState(0);
   const currentCardData = tutorialData[step];
 
+  const nextStep = () => {
+    setStep((prevStep) => (prevStep < tutorialData.length - 1 ? prevStep + 1 : prevStep));
+  };
+
   return (
     <div>
-      <Card title={currentCardData.title} description={currentCardData.description} />
+      <Card 
+      title={currentCardData.title} 
+      description={currentCardData.description} 
+      nextStep={nextStep}
+      />
+
     </div>
   );
 }
--- Sprint-5/src/Card.jsx
+++ Sprint-5/src/Card.jsx
@@ -1,8 +1,9 @@
-function Card({ title, description }) {
+function Card({ title, description, nextStep }) {
   return (
     <div>
       <h2>{title}</h2>
       <p>{description}</p>
+      <button onClick={nextStep}>Següent passa</button>
     </div>
   );
 }
